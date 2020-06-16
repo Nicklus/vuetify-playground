83eba5a1968d8a0f0de22b834840b51c387955de
Nicooo <saoutn@gmail.com>
2020-06-16 16:46:00 +0200

    rajout v-app exemple

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,62 @@
 <template>
   <v-app>
-    <v-app-bar
-      color="deep-purple accent-4"
-      dense
+    <v-navigation-drawer
+      v-model="drawer"
+      :color="color"
+      :expand-on-hover="expandOnHover"
+      :mini-variant="miniVariant"
+      :right="right"
+      :permanent="permanent"
+      :src="bg"
+      absolute
       dark
     >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Page title</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
+      <!-- -->
+    </v-navigation-drawer>
 
-      <v-menu
-        left
-        bottom
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            icon
-            v-bind="attrs"
-            v-on="on"
-          >
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item
-            v-for="n in 5"
-            :key="n"
-            @click="() => {}"
-          >
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+    <v-app-bar app>
+      <!-- -->
     </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <!-- <router-view></router-view> -->
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
   </v-app>
 </template>
 
 <script>
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  data() {
+    return {
+      background: false,
+      color: 'primary',
+      drawer: true,
+      expandOnHover: false,
+      miniVariant: false,
+      permanent: true,
+      right: false
+    };
+  },
+  computed: {
+    bg() {
+      return this.background
+        ? 'https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg'
+        : undefined;
+    }
+  }
 };
 </script>
 
